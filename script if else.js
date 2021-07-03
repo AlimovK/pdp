eed5db13c332af78a9cdf9ccd9697e692146a5b2
k-737 <kamronalimov80@gmail.com>
2021-07-04 03:08:13 +0500

    java script lesson

---
+++ script if else.js
@@ -0,0 +1,11 @@
+var grade = 85
+
+if (grade => 95) {
+    console.log(`your score A+`)
+} 
+else if (grade => 90 ){
+    console.log(`your score A`)
+}
+else {
+    console.log(`you failed`)
+}
---
+++ script-lesson1.js
@@ -0,0 +1,57 @@
+// var a = 2
+// p = 4*a 
+// console.log(p);
+
+// var a = 5
+// s = 5**2
+// console.log(s);
+
+// var a = 3
+// var b = 4
+// s = a*b
+// p = 2*(a+b)
+// console.log(s,p);
+
+// var d = 5
+// n = 3.14
+// l = d*n
+// console.log(l);
+
+// var a = 5
+// v = a**3
+// s = 6*a**2
+// console.log(v,s);
+
+//  a = 4
+//  b = 6
+//  c = 8
+//  v = a*b*c
+//  S=2*(a*b+b*c+a*c)
+//  console.log(v, S);
+
+// r = 9
+// p = 5
+// L = 2*p*r
+// s = p*(r**2)
+// console.log(L,s);
+
+// a = 7
+// b = 8
+// j = (a+b)/2
+// console.log(j);
+
+// a = 8
+// b = 8
+// console.log(Math.sqrt(a*b)/2)
+
+// a = 4
+// b = 6
+// console.log(a+b, a*b, a**2, b**2);
+
+// a = 7
+// b = 9
+// a = a**2
+// b = b**2
+// c = Math.sqrt(a+b)
+// p = a+b+c
+// console.log(p);
