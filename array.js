d798f9cc96125e5f7faf279d04513eaff3e64e04
k-737 <kamronalimov80@gmail.com>
2021-07-05 15:12:25 +0500

    javascript-lessons

---
+++ array.js
@@ -0,0 +1,16 @@
+// var number = [1,2,3,4,5,6,7,8,9,10,0,12,11,13,14]
+// for (let i = 1; i < number.length; i++ ) {
+//     if (i%3===0 ){
+//         console.log(i, `-3`);
+//     } else if (i%2===0){
+//         console.log(i, `-2`);
+//     }
+// }
+
+
+// var arr = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16]
+// for (let i = 1; i < arr.length; i++){
+//  if(i%2===0){
+//      console.log(i);
+//  }
+// }
