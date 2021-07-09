f4b0c92c0e0725bf5ad2c972a4156ef914ac8a8f
k-737 <kamronalimov80@gmail.com>
2021-07-09 16:31:36 +0500

    hometasks

---
+++ script-lesson-obj.js
@@ -0,0 +1,91 @@
+//todo array bor arrayni ichida obj bor  obj ni xammasiga surname qo'shish kerak 
+
+// var student = [
+//     {
+//         name: `Kamron`,
+//         status: `student`,
+//         age: `21`
+//     },
+//     {
+//         name: `Alisher`,
+//         status: `developer`,
+//         age: `25`
+//     },
+//     {
+//         name: `Samandar`,
+//         status: `plumber`,
+//         age: `32`
+//     }
+// ];
+// student.map((value, index) => {
+//     student[0].surname = `Alimov`
+//     student[1].surname = `Aliev`
+//     student[2].surname = `Xoliqov`
+//     console.log(value);
+})
+
+//todo (if,for, ?, function, array and object dan foydalanib)
+//todo array bor uni ichida obj bor id, name, age. shundan 10ta bor. find(argument) degan funksyani ichida bo'ladi argumentni o'rniga sortage deb yozadigan bo'lsak age ni so'rt qilib berishi kerak.
+//todo 1) age sort kamayish tartibiga qarab
+//todo 2) sort name
+//todo 3) sort id 
+
+
+var list = [
+    {
+    id: 1,
+    name: `Kamron`,
+    age: 21
+},
+{
+    id: 2,
+    name: `Alisher`,
+    age: 24
+},
+{
+    id: 3,
+    name: `Sanjar`,
+    age: 25
+},
+{
+    id: 4,
+    name: `Abdulloh`,
+    age: 18
+},
+{
+    id: 5,
+    name: `Abubakr`,
+    age: 20
+},
+{
+    id: 6,
+    name: `Ali`,
+    age: 28
+},
+{
+    id: 7,
+    name: `Mamur`,
+    age: 21
+},
+{
+    id: 8,
+    name: `Bekzod`,
+    age: 23
+},
+{
+    id: 9,
+    name: `Azamat`,
+    age: 34
+},
+{
+    id: 10,
+    name: `Muzaffar`,
+    age: 21
+},
+];  
+// list.sort((a,b)=>b.id-a.id)
+// console.log(list);
+
+
+
+
